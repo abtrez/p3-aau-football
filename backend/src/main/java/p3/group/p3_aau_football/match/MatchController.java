f777059c51313126275aa7c87b63be3ff896ee16
Oliver Dalgaard <oliver@ihd.dk>
2025-10-30 15:12:18 +0100

    Fixed edit patch mapping

--- backend/src/main/java/p3/group/p3_aau_football/match/MatchController.java
+++ backend/src/main/java/p3/group/p3_aau_football/match/MatchController.java
@@ -1,13 +1,14 @@
 package p3.group.p3_aau_football.match;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.service.annotation.PostExchange;
-
-import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController // flags class, so it is ready for use by Spring MVC to handle web requests.
 @RequestMapping("/api/match")
@@ -24,19 +25,15 @@ public class MatchController {
         return new Match(id, id);
     }
 
-    @PostMapping("/{id}/edit")
-    public String editMatch(@PathVariable("id") String id, @RequestParam String test1) {
-        return test1;
+    @PatchMapping("/{id}/edit")
+    public String editMatch(@PathVariable("id") String id, @RequestParam("test1") String test1, @RequestParam("test2") String test2) {
+        return test1 + " " + test2;
     }
 
-    @PostMapping("/add")
+    /* @PostMapping("/add")
     public ResponseEntity<String> test() {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("Content-Type", "application/json");
         return new ResponseEntity<String>("Created", responseHeaders, HttpStatus.OK);
-    }
-
-
-
-
+    } */
 }
