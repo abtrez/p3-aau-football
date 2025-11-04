8400e6da29b3b72cd2157b30b937309754c20ab4
Oliver Dalgaard <oliver@ihd.dk>
2025-11-04 15:08:38 +0100

    Added database functionality

---
+++ backend/src/main/java/p3/group/p3_aau_football/match/MatchRepository.java
@@ -0,0 +1,7 @@
+package p3.group.p3_aau_football.match;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MatchRepository extends MongoRepository<Match, String> {
+
+}
--- backend/src/main/java/p3/group/p3_aau_football/match/MatchService.java
+++ backend/src/main/java/p3/group/p3_aau_football/match/MatchService.java
@@ -1,4 +1,29 @@
 package p3.group.p3_aau_football.match;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MatchService {
+
+    @Autowired
+    MatchRepository matchRepository;
+
+    public List<Match> getOverview() {
+        return matchRepository.findAll();
+    }
+
+    public Optional<Match> getMatch(String id) {
+        return matchRepository.findById(id);
+    }
+
+    public Match insertMatch(String homeTeam, String awayTeam) {
+        Match insertedMatch = new Match(homeTeam, awayTeam);
+
+        return matchRepository.insert(insertedMatch);
+    }
+
 }
