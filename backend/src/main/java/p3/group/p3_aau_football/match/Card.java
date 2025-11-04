83c75a42042ed1a5f3c74c827d58691c798d6ad0
Abtin Rezvani <151622443+abtrez@users.noreply.github.com>
2025-11-04 14:17:36 +0100

    Initial addition of Competition and Season class

--- backend/src/main/java/p3/group/p3_aau_football/match/Card.java
+++ backend/src/main/java/p3/group/p3_aau_football/match/Card.java
@@ -1,5 +1,5 @@
 package p3.group.p3_aau_football.match;
 
 public class Card extends MatchEvent {
-    private Color;
+    private CardType;
 }
--- backend/src/main/java/p3/group/p3_aau_football/match/Match.java
+++ backend/src/main/java/p3/group/p3_aau_football/match/Match.java
@@ -3,19 +3,32 @@ package p3.group.p3_aau_football.match;
 
 import p3.group.p3_aau_football.team.Team;
 
+import java.util.List;
+
 public class Match {
     private int id;
-    private Team homeTeam;
-    private Team awayTeam;
+    private final Team homeTeam;
+    private final Team awayTeam;
 
+    //Score & Events
+    private int homeScore;
+    private int awayScore;
+    List<MatchEvent> matchEvents;
 
+    //Details
+    /*
+    (dateTime) date and time - kick-offtime splittes?
+    Venue venue
+    List<Referee> referees
+    int squadSize
+    */
 
-
-
+    //Constructor, should possibly have date
     public Match(Team homeTeam, Team awayTeam) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
     }
+
 
     public Team getHomeTeam(){
         return this.homeTeam;
