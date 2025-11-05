f7d7875a9cb31b7eed59acd5eff591da430bd8a2
Oliver Dalgaard <oliver@ihd.dk>
2025-11-05 14:13:50 +0100

    Fixed another import error

--- frontend/src/app/teams/page.tsx
+++ frontend/src/app/teams/page.tsx
@@ -1,10 +1,10 @@
-import TeamCard from "@/app/components/Team/TeamCard";
+import TeamCard from "@/components/Team/TeamCard";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl text-center">Teams Overview</h1>
-      <TeamCard />
+      {/* <TeamCard /> */}
     </>
   );
 }
