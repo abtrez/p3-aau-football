16960a1f970c615acfbce1a1bf56d029a6b467e0
kresten-j <kads24@student.aau.dk>
2025-11-06 12:12:06 +0100

    fixed TeamCardInterface to match teams.json

--- frontend/src/components/team/TeamOverview.tsx
+++ frontend/src/components/team/TeamOverview.tsx
@@ -1,6 +1,8 @@
 import teams from "@/data/teams.json";
 import teamCard, { TeamCardInterface } from "@/components/team/TeamCard";
 
-const footballTeams = teams as TeamCardInterface;
+const footballTeams = teams as TeamCardInterface[];
 
-export default function TeamCard() {}
+export default function TeamCard() {
+  return <div></div>;
+}
