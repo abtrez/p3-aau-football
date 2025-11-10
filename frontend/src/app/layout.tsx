93303fbf8fa7e1ab863a09dfbe98d26fb3d8249a
kresten-j <kads24@student.aau.dk>
2025-11-10 11:13:25 +0100

    AppRouterCacheProvider added. Prevents hydration errors

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import BottomBarNavigation from "@/components/navigation/BottomBarNavigation";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <BottomBarNavigation />
+        <AppRouterCacheProvider>
+          {children}
+          <BottomBarNavigation />
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
