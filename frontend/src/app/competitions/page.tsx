ff6a766cbdeccc3ba437126b9a0f18c977934846
kresten-j <kads24@student.aau.dk>
2025-11-10 12:14:16 +0100

    added pages for the navigation links

--- frontend/src/app/competitions/page.tsx
+++ frontend/src/app/competitions/page.tsx
@@ -3,8 +3,8 @@ import TeamOverview from "@/components/team/TeamOverview";
 export default function Page() {
   return (
     <>
-      <h1 className="text-3xl text-center mb-3">Teams Overview</h1>
-      <TeamOverview />
+      <h1 className="text-3xl text-center mb-3">Competitions</h1>
+      {/* <TeamOverview /> */}
     </>
   );
 }
