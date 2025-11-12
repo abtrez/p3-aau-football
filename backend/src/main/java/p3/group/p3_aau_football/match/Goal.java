1e898f4ef04746f23a4ec9d0cbc105a196cf5294
Abtin Rezvani <151622443+abtrez@users.noreply.github.com>
2025-11-12 09:41:44 +0100

    fixed Player class imports, in MatchEvent.java(missing) and Goal.java(capital R to lowercase, not updated after  role folder renamed).

--- backend/src/main/java/p3/group/p3_aau_football/match/Goal.java
+++ backend/src/main/java/p3/group/p3_aau_football/match/Goal.java
@@ -1,6 +1,6 @@
 package p3.group.p3_aau_football.match;
 
-import p3.group.p3_aau_football.Role.Player;
+import p3.group.p3_aau_football.role.Player;
 
 public class Goal extends MatchEvent {
     private Player assister;
