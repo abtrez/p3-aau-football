0b53f1fb40675ae3698a1a251f8260a81a6f7de7
W1llbam <williammedstilredbull@gmail.com>
2025-11-12 18:16:01 +0100

    Attributes for player and league statistics

---
+++ backend/src/main/java/p3/group/p3_aau_football/statistic/LeagueStatistic.java
@@ -0,0 +1,82 @@
+package p3.group.p3_aau_football.statistic;
+
+public class LeagueStatistic extends Statistics {
+    private String teamId;
+    private int matchesPlayed;
+    private int wins;
+    private int draws;
+    private int losses;
+    private int goalsFor;
+    private int goalsAgainst;
+    private int points;
+
+
+    public int calculatePoints(LeagueStatistic leagueStats) {
+        return (leagueStats.wins * 3) + (leagueStats.draws);
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getTeamId() {
+        return this.teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
+    public void setMatchesPlayed(int matchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    public int getMatchesPlayed() {
+        return this.matchesPlayed;
+    }
+
+    public int getWins() {
+        return this.wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getDraws() {
+        return this.draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getLosses() {
+        return this.losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public void setGoalsFor(int goalsFor) {
+        this.goalsFor = goalsFor;
+    }
+
+    public int getGoalsFor() {
+        return this.goalsFor;
+    }
+
+    public int getGoalsAgainst() {
+        return this.goalsAgainst;
+    }
+
+    public void setGoalsAgainst(int goalsAgainst) {
+        this.goalsAgainst = goalsAgainst;
+    }
+
+}
