36dd0a1252f121567243df8f6e3c4be205a1d675
kresten-j <kads24@student.aau.dk>
2025-11-13 14:44:13 +0100

    Venue is now a collection

--- backend/src/main/java/p3/group/p3_aau_football/match/Venue.java
+++ backend/src/main/java/p3/group/p3_aau_football/match/Venue.java
@@ -1,7 +1,9 @@
 package p3.group.p3_aau_football.match;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "venues")
 public class Venue {
     @Id
     String id;
