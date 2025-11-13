525f56cb3f71c22ae30a867f4c78091f36520dd2
W1llbam <williammedstilredbull@gmail.com>
2025-11-13 14:52:10 +0100

    Added two exceptions

---
+++ backend/src/main/java/p3/group/p3_aau_football/statistic/exception/DocumentNotFoundException.java
@@ -0,0 +1,7 @@
+package p3.group.p3_aau_football.statistic.exception;
+
+public class DocumentNotFoundException extends RuntimeException {
+    public DocumentNotFoundException(String message) {
+        super(message);
+    }
+}
