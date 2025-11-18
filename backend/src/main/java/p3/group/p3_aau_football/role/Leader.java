8ab56121219597f9af88ca9ef3480df8483f3604
Oliver Dalgaard <87284361+oliverdalgaard@users.noreply.github.com>
2025-11-18 12:38:16 +0100

    Remove unused import in Leader.java
    Removed unused import for DocumentReference.

--- backend/src/main/java/p3/group/p3_aau_football/role/Leader.java
+++ backend/src/main/java/p3/group/p3_aau_football/role/Leader.java
@@ -1,7 +1,5 @@
 package p3.group.p3_aau_football.role;
 
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
-
 import p3.group.p3_aau_football.permission.Permission;
 
 public class Leader extends Role implements Permission {
