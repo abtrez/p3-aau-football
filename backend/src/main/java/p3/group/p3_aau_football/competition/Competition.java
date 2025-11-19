50bd8368419cf08cacc3f9d15a123a547c998639
kresten-j <kads24@student.aau.dk>
2025-11-19 15:26:29 +0100

    We reverted to just having a competition class with a name

--- backend/src/main/java/p3/group/p3_aau_football/competition/Competition.java
+++ backend/src/main/java/p3/group/p3_aau_football/competition/Competition.java
@@ -1,18 +1,11 @@
 package p3.group.p3_aau_football.competition;
 
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
-import p3.group.p3_aau_football.match.Match;
-import p3.group.p3_aau_football.season.Season;
-import p3.group.p3_aau_football.team.Team;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
-public abstract class Competition {
+@Document(collection = "competitions")
+public class Competition {
+    @Id
+    private String id;
     protected String name;
-    @DocumentReference
-    protected Season season;
-    @DocumentReference
-    protected List<Team> participatingTeams;
-    @DocumentReference
-    protected List<Match> matches;
 }
