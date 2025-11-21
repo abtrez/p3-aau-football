4da4ff04050431577ba92d85e5993576ddca95a2
Abtin Rezvani <151622443+abtrez@users.noreply.github.com>
2025-11-21 11:20:40 +0100

    undid last commit, and resolved comments on merge request about unused import. Prior issue with mismatching ids was due data in mongodb with field named 'id' instead of '_id'. Constructor was correct, spring data could not recognize mapping with the earlier field name

--- backend/src/main/java/p3/group/p3_aau_football/match/event/Goal.java
+++ backend/src/main/java/p3/group/p3_aau_football/match/event/Goal.java
@@ -1,8 +1,5 @@
 package p3.group.p3_aau_football.match.event;
 
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
-import p3.group.p3_aau_football.role.Player;
-
 public class Goal extends MatchEvent {
     private String assisterId; //Optional
 
--- backend/src/main/java/p3/group/p3_aau_football/match/event/MatchEvent.java
+++ backend/src/main/java/p3/group/p3_aau_football/match/event/MatchEvent.java
@@ -2,6 +2,7 @@ package p3.group.p3_aau_football.match.event;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.bson.types.ObjectId;
 
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
@@ -16,21 +17,17 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 public abstract class MatchEvent {
 
-    private String eventId;
+    private String id;
     private String playerId; //Optional. Don't force teams to log
     private String teamId; //If player null, must know what team event belongs to
     private Integer minute; //Optional. Integer wrapper class to allow null, rather than primitive int that defaults to 0.
 
     public MatchEvent() {
-        //this.id = new ObjectId().toHexString();
+        this.id = new ObjectId().toHexString();
     }
 
-    public String getEventId() {
-        return this.eventId;
-    }
-
-    public void setEventId(String eventId) {
-        this.eventId = eventId;
+    public String getId() {
+        return this.id;
     }
 
     public String getPlayerId() {
