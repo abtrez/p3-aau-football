4170d86035640b00459a7b4e4ab5bf73ccd0c30c
W1llbam <williammedstilredbull@gmail.com>
2025-11-24 09:58:40 +0100

    added controllers for each statistics + StatisticsController interface and refactored code accordingly

---
+++ backend/src/main/java/p3/group/p3_aau_football/statistic/player/PlayerStatisticsController.java
@@ -0,0 +1,6 @@
+package p3.group.p3_aau_football.statistic.player;
+
+import p3.group.p3_aau_football.statistic.common.StatisticsController;
+
+public class PlayerStatisticsController implements StatisticsController {
+}
