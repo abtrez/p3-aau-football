0345f7c85deb122cd9cc6992ec359c3aaa59e248
W1llbam <williammedstilredbull@gmail.com>
2025-11-25 14:37:46 +0100

    implemented detailed exception handling for Compeition Package

--- backend/src/main/java/p3/group/p3_aau_football/competition/CompetitionRepository.java
+++ backend/src/main/java/p3/group/p3_aau_football/competition/CompetitionRepository.java
@@ -3,4 +3,5 @@ package p3.group.p3_aau_football.competition;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CompetitionRepository extends MongoRepository<Competition, String> {
+    boolean existsBySeasonAndName(String season, String name);
 }
--- backend/src/main/java/p3/group/p3_aau_football/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/p3/group/p3_aau_football/exceptions/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import p3.group.p3_aau_football.competition.CompetitionAlreadyExistsException;
+import p3.group.p3_aau_football.competition.CompetitionNotFoundException;
 import p3.group.p3_aau_football.statistic.league.LeagueStatisticsNotFoundException;
 import p3.group.p3_aau_football.statistic.player.PlayerStatisticsNotFoundException;
 
@@ -38,5 +40,30 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
-    
+
+    @ExceptionHandler(CompetitionNotFoundException.class)
+    public ResponseEntity<ApiError> handleCompetitionsNotFound(
+            CompetitionNotFoundException exception, HttpServletRequest request
+    ) {
+        ApiError error = new ApiError(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(CompetitionAlreadyExistsException.class)
+    public ResponseEntity<ApiError> handleCompetitionAlreadyExists(
+            CompetitionAlreadyExistsException exception, HttpServletRequest request
+    ) {
+        ApiError error = new ApiError(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
 }
