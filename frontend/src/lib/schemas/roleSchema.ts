54681f8604b62b45a38c0a398a6fa75f94a85b8e
valentino995 <vtrone24@student.aau.dk>
2025-11-25 13:01:46 +0100

    roleschema
    create leader and coach schema

--- frontend/src/lib/schemas/roleSchema.ts
+++ frontend/src/lib/schemas/roleSchema.ts
@@ -2,14 +2,28 @@ import { z } from "zod";
 
 export const playerRoleSchema = z.object({
     type: z.literal("PLAYER"),
+    name:z.string(),
     shirtNumber: z.number().int().nonnegative(),
     positionGroup: z.enum(["DEF", "MID", "FOW"]),
     position: z.enum(["CB", "LB", "RB", "LWB", "RWB", "CDM", "CM", "CAM",
         "LM", "ST", "CF", "LW", "RW", "LA", "RA"])
 });
 
+export const leaderRoleSchema = z.object ({
+    type: z.literal("LEADER"),
+    name:z.string().nullable().optional()
+})
+
+export const coachRoleSchema = z.object ({
+    type: z.literal("COACH"),
+    name: z.string().nullable().optional(),
+    isAssistant: z.boolean().nullable().optional()
+})
+
 export const roleSchema = z.discriminatedUnion("type", [
-    playerRoleSchema
+    playerRoleSchema,
+    leaderRoleSchema,
+    coachRoleSchema
 ]);
 
 export type Role = z.infer<typeof roleSchema>;
